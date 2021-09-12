4baba2cf3437322d712cb04673f6a3701f77f13b
johnson <644128256@qq.com>
2021-09-12 17:12:40 +0800

    testidea3

---
+++ test/src/com/zs/test2.java
@@ -0,0 +1,13 @@
+package com.zs;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author John
+ * @create 2021-09-1217:06
+ */
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
