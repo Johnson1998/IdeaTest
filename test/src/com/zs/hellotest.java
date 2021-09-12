270ab4f87a3e81c752aa2452d5637fe960c031ab
johnson <644128256@qq.com>
2021-09-12 17:03:24 +0800

    testidea

---
+++ test/src/com/zs/hellotest.java
@@ -0,0 +1,11 @@
+package com.zs;
+
+/**
+ * @author John
+ * @create 2021-09-1217:02
+ */
+public class hellotest {
+    public static void main(String[] args) {
+        System.out.println("this is idea project");
+    }
+}
